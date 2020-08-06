c948562428e31ce279694a249f06e741590c5e18
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-06 23:48:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
-  if array[index] == value_to_find
-    return index 
+  array.length.times do |i|
+  if array[i] == value_to_find
+    return i 
   end
 end
